8a1fa77dd1ca113b8daf311dcdd24c78df64adf3
gestacio <gestacio1310@gmail.com>
2023-05-29 16:02:08 -0400

    a casita

--- frontend/src/app/components/caja/caja.component.ts
+++ frontend/src/app/components/caja/caja.component.ts
@@ -11,6 +11,10 @@ import { ProductService } from 'src/app/services/product.service';
 export class CajaComponent implements OnInit {
   listProducts: Product[] = []
   loading: boolean = false;
+  listCart: Product[] = []
+  cartCostoNeto = 0;
+  cartIva = 0;
+  cartTotal = 0;
 
   constructor(private _productService: ProductService, private toastr: ToastrService) {}
 
@@ -24,6 +28,46 @@ export class CajaComponent implements OnInit {
       this.listProducts = data;
       this.loading = false;
     })
+  }
+
+  insertListCart(product: Product) {
+    this.listCart.push(product);
+    this.calcularCostos(this.listCart);
+  }
+
+  deleteListCart(product: Product) {
+    var index = this.listCart.indexOf(product)
+    // this.restarCostos(product);
+    this.listCart.splice(index, 1);
+    this.calcularCostos(this.listCart);
+  }
+
+  calcularCostos(listaCart: Product[]) {
+    this.cartCostoNeto = 0;
+    this.cartIva = 0;
+    this.cartTotal = 0;
+
+    for (let i = 0; i < listaCart.length; i++) {
+      const product = listaCart[i];
+
+      this.cartCostoNeto = this.cartCostoNeto + product.price;
+      this.cartIva = this.cartCostoNeto * 0.16;
+      this.cartTotal = this.cartCostoNeto + this.cartIva;
+    }
+  }
+
+  clearListCart() {
+    this.listCart.length = 0;
+    this.calcularCostos(this.listCart);
+  }
+
+  confirmarCompra() {
+    let text;
+    if (confirm("Press a button!") == true) {
+      alert("apretaste ok");
+    } else {
+      text = "You canceled!";
+    }
   }
 
   // deleteProduct(id: number) {
